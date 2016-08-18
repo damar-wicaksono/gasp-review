f9a5d1243e584fd6195e7decfc8c8f2d6111a91c
Damar Wicaksono <damar.wicaksono@gmail.com>
2016-08-18 16:45:26 +0200

    Implement the dampened cosine test function in R

---
+++ simulation/r_test_functions/cos_dampened.R
@@ -0,0 +1,20 @@
+#' Calculate the Dampened Cosine Test Function
+#'
+#' Reference:
+#'   T. Santner, B. Williams, and W. Notz, "The Design and Analysis of 
+#    Computer Experiments," pp. 64, Springer-Verlag, New York, 2003.
+#' 
+#' @param x A vector of float [0, 1]. The argument.
+#' @return the function values evaluated at x
+cos_dampened <- function(x) {
+    if (missing(x)) {
+        stop("Function needs argument with values [0,1]")
+    } else if (any(x > 1.0)) {
+        stop("Function argument greater than 1.0, the domain is [0,1]")
+    } else if (any(x < 1.0)) {
+        stop("Function argument smaller than 1.0, the domain is [0,1]")
+    }
+
+    y <- exp(1)^(-1.4 * x) * cos(3.5 * pi * x)
+    return(y)
+}
