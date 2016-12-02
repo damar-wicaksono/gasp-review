63867c2778c52842c53537bfcddddae99b720cb6
Damar Wicaksono <damar.wicaksono@psi.ch>
2016-12-02 09:32:43 +0100

    Update header comment for the q2 function

--- analysis/r-scripts/q2.R
+++ analysis/r-scripts/q2.R
@@ -1,7 +1,10 @@
 #' Calculate the Coefficient of Determination (Q2) of a meta-model prediction
 #' 
 #' Given the km object, test input data, test output data compute the 
-#' coefficient of determination
+#' coefficient of determination.
+#' 
+#' Make sure that the test input dataframe has the same column names as the 
+#' one used for training
 #'
 #' Reference:
 #'   B. Iooss, L. Boussouf, V. Feuillard, and A. Marrel, "Numerical Studies of 
@@ -9,13 +12,13 @@
 #'   Advances in Systems and Measurements, vol. 3, pp. 11-21, 2010.
 #' 
 #' @param km_object the Gaussian process metamodel object from DiceKriging
-#' @param x_test the test input dataframe
+#' @param x_test the test inputs (dataframe)
 #' @param y_test the test output data set (a vector)
 #' @return the coefficient of determination
 q2 <- function(km_object, x_test, y_test ) {
    y_predict <- predict(km_object, newdata = x_test, "UK")$mean
-   ss_res <- sum((y_test - y_predict)^2)            # residual sum of squares
-   ss_tot <- sum((y_test - mean(y_test))^2) # total sum of squares
+   ss_res <- sum((y_test - y_predict)^2)        # residual sum of squares
+   ss_tot <- sum((y_test - mean(y_test))^2)     # total sum of squares
    
    return(1 - (ss_res/ss_tot))
 }
