8261ce882842da2e27887b7a7c88e0a146cb50b8
Damar Wicaksono <damar.wicaksono@psi.ch>
2017-04-13 20:02:30 +0200

    Update analysis on borehole and piston model

--- analysis/borehole.R
+++ analysis/borehole.R
@@ -1,85 +1,174 @@
 #
-# title     :
+# title     : borehole.R
 # purpose   : Analysis of Borehole function using Gaussian Process
-# author    :
-# date      :
+# author    : WD41
+# date      : 13.04.2017
 #
-
+# Load Required Libraries -----------------------------------------------------
+library(DiceKriging)
+library(ggplot2)
 source("../simulation/r_test_functions/borehole.R")
+
+# Load Required Data ----------------------------------------------------------
 xx_init <- read.csv("../simulation/design/8params/srs_16_8.csv", header = F)
-xx_pool  <- read.csv("../simulation/design/8params/lhs_500_8.csv", header = F)
+xx_tests  <- read.csv("../simulation/design/8params/lhs_500_8.csv", header = F)
+names(xx_tests) <- input_names
+xx_pool_resc <- data.frame(rescaleInputBorehole(xx_pool))
+y_tests <- evalOutputBorehole(rescaleInputBorehole(xx_tests))
 
 set.seed(3733)
-cov_types <- c("exp", "gauss", "powexp", "matern3_2", "matern5_2")
-reg_forms <- c("~0", "~.", "~.^2")
-reg_names <- c("constant", "full_linear", "full_linear+int")
-num_smpls <- c(100, 200, 400, 800)
-doe_names <- c("srs", "lhs", "lhs-opt", "sobol")
 
+
+# The modeling choices --------------------------------------------------------
+cov_types <- c("exp", "gauss", "powexp", "matern3_2", "matern5_2")
+reg_forms <- c("~0", "~.") #, "~.^2")
+reg_names <- c("constant", "full_linear", "full_linear+int")
+num_smpls <- c(16, 32, 64, 128)
+doe_names <- c("srs", "lhs", "lhs-opt", "sobol")
+doe_names <- c("srs", "lhs", "sobol")
 n_reps <- 25
 input_names <- c()
-for (i in 1:8) input_names <- c(input_names, paste("x",i,sep=""))
-q2_borehole <- data.frame(q2 = c(), 
-                          cov_type = c(),
-                          doe = c(),
-                          reg = c(),
-                          n = c())
 
-for (doe_name in doe_names)
+transf_names <- c("original", "log", "sqrt")
+
+input_names <- c()
+for (i in 1:8) input_names <- c(input_names, paste("x", i, sep=""))
+
+# Initialize dataframe --------------------------------------------------------
+err_borehole <- data.frame(rmse = c(),
+                           q2 = c(),
+                           cov_type = c(),
+                           doe = c(),
+                           reg = c(),
+                           n = c(),
+                           transf = c())
+
+# Loop over modeling choices to construct metamodel ---------------------------
+# Loop over type of output transformation, number of training samples, 
+# type of design, regression term, covariance types, and replications. 
+# Note that for the regression term, the full interaction model is fitted only
+# for the last two sample sizes, 64 and 128.
+# Fewer number of samples are known to fail
+for (transf_name in transf_names)
 {
-    # Read input and a pool test data
-    init_fname <- paste("../simulation/design/8params/", doe_name, "_64_8.csv", sep = "")
-    xx_init <- read.csv(init_fname, header = F)
-    for (i in 1:length(reg_forms))
+    if (transf_name == "log")
     {
-        for (cov_type in cov_types)
+        transf <- log
+        inv_transf <- exp
+    } else if (transf_name == "sqrt")
+    {
+        trasnf <- sqrt
+        inv_transf <- power
+    }
+    
+    for (num_smpl in num_smpls)
+    {
+        for (doe_name in doe_names)
         {
-            for (j in 1:n_reps)
+            # Loop over regression term
+            for (i in 1:length(reg_forms))
             {
-                # Sample from test data to complete test input
-                xx_train <- rbind(xx_init, xx_pool[sample(1:500, 0.32*64),])
-                
-                # Sample from the pool test data
-                xx_tests <- xx_pool[sample(1:500, 450),]
-                
-                # Rescale the input
-                xx_train_resc <- data.frame(rescaleInputBorehole(xx_train))
-                xx_tests_resc <- data.frame(rescaleInputBorehole(xx_tests))
-                names(xx_train_resc) <- input_names
-                names(xx_tests_resc) <- input_names
-                
-                # Evaluate the output based on training and test data
-                y_train <- evalOutputPiston(xx_train_resc)
-                y_tests <- evalOutputPiston(xx_tests_resc)
-                
-                # Train the model
-                m <- km(as.formula(reg_forms[1]), 
-                        design = xx_train, response = y_train, 
-                        covtype = "exp",
-                        control = list(pop.size = 100, trace = F))
-                
-                # Make prediction using metamodel
-                y_preds <- predict(m, newdata=xx_tests, type="UK")$mean
-                
-                # Compute Predictivity Coefficient
-                q2_borehole <- rbind(q2_borehole,
-                                     data.frame(q2 = evalErmse(y_tests, y_preds), 
-                                                cov_type = cov_type,
-                                                doe = doe_name,
-                                                reg = reg_names[i],
-                                                n = 32))    
+                # Loop over covariance type
+                for (cov_type in cov_types)
+                {
+                    # Loop over replications
+                    for (j in 1:n_reps)
+                    {
+                        # Read input
+                        train_filename <- paste(doe_name, "_", num_smpl, 
+                                                "_8_", j, ".csv", 
+                                                sep = "")
+                        train_fullname <- paste("../simulation/design/8params/", 
+                                                train_filename, 
+                                                sep = "")
+                        xx_train <- read.csv(train_fullname, header = F)
+                        # Evaluate output with rescaled input
+                        y_train <- evalOutputBorehole(rescaleInputBorehole(xx_train))
+                        # Give names to the input dataset
+                        names(xx_train) <- input_names
+                        
+                        # Train the model
+                        m <- km(as.formula(reg_forms[i]),
+                                design = xx_train, response = transf(y_train), 
+                                covtype = cov_type,
+                                control = list(pop.size = 100, trace = T))
+                        
+                        # Make prediction using metamodel
+                        y_preds <- predict(m, newdata=xx_tests, type="UK")$mean
+                        
+                        # Compute Predictivity Coefficient
+                        err_borehole <- rbind(err_borehole,
+                                              data.frame(
+                                                  rmse = evalErmse(y_tests, 
+                                                                   inv_transf(y_preds)),
+                                                  q2 = evalQ2(y_tests, 
+                                                              inv_transf(y_preds)),
+                                                  cov_type = cov_type,
+                                                  doe = doe_name,
+                                                  reg = reg_names[i],
+                                                  n = num_smpl,
+                                                  transf = transf_name)) 
+                        
+                    }
+                }
             }
+            
         }
     }
 }
 
-p <- ggplot(data = q2_borehole)
-p <- p + geom_jitter(mapping = aes(x=cov_type, y = q2), width=0.10) + facet_grid(doe ~ reg)
-p <- p + geom_point(mapping = aes(x=cov_type, y = q2)) + facet_grid(reg ~ doe)
-p
+for (num_smpl in num_smpls)
+{
+    for (doe_name in doe_names)
+    {
+        # Loop over regression term
+        for (i in 1:length(reg_forms))
+        {
+            # Loop over covariance type
+            for (cov_type in cov_types)
+            {
+                # Loop over replications
+                for (j in 1:n_reps)
+                {
+                    # Read input
+                    train_filename <- paste(doe_name, "_", num_smpl, 
+                                            "_8_", j, ".csv", 
+                                            sep = "")
+                    train_fullname <- paste("../simulation/design/8params/", 
+                                            train_filename, 
+                                            sep = "")
+                    xx_train <- read.csv(train_fullname, header = F)
+                    # Evaluate output with rescaled input
+                    y_train <- evalOutputBorehole(rescaleInputBorehole(xx_train))
+                    # Give names to the input dataset
+                    names(xx_train) <- input_names
+                    
+                    # Train the model
+                    m <- km(as.formula(reg_forms[i]),
+                            design = xx_train, response = log(y_train), 
+                            covtype = cov_type,
+                            control = list(pop.size = 100, trace = T))
+                    
+                    # Make prediction using metamodel
+                    y_preds <- predict(m, newdata=xx_tests, type="UK")$mean
+                    
+                    # Compute Predictivity Coefficient
+                    err_borehole <- rbind(err_borehole,
+                                          data.frame(
+                                              rmse = evalErmse(y_tests, 
+                                                               exp(y_preds)),
+                                              q2 = evalQ2(y_tests, 
+                                                          exp(y_preds))
+                                                    cov_type = cov_type,
+                                                    doe = doe_name,
+                                                    reg = reg_names[i],
+                                                    n = num_smpl)) 
+                    
+                }
+            }
+        }
+        
+    }
+}
 
-
-init_fname <- paste("../simulation/design/8params/", "lhs-opt", "_64_8.csv", sep = "")
-xx_init2 <- read.csv(init_fname, header = F)
-plot(xx_init1[,1], xx_init1[,2])
-plot(xx_init2[,1], xx_init2[,2])
+saveRDS(err_borehole, "err_borehole.Rds")
--- analysis/piston.R
+++ analysis/piston.R
@@ -5,18 +5,14 @@
 # date      :
 #
 
-# ----
+# Load Required Libraries -----
 library(DiceKriging)
 library(ggplot2)
+
 source("../simulation/r_test_functions/piston.R")
 input_names <- c("M", "S", "V0", "k", "P0", "Tamb", "T0")
 
-# Q2 function -----------
-evalQ2 <- function(y_test, y_pred)
-{
-    q2 <- 1 - sum((y_test - y_pred)^2) / sum((y_test - mean(y_test))^2)
-    return(q2)
-}
+
 
 # Ermse function -----------
 evalErmse <- function(y_test, y_pred)
@@ -29,21 +25,114 @@ evalErmse <- function(y_test, y_pred)
 input_names <- c()
 for (i in 1:7) input_names <- c(input_names, paste("x",i,sep=""))
 xx_init <- read.csv("../simulation/design/7params/srs_64_7.csv", header = F)
-xx_pool  <- read.csv("../simulation/design/7params/lhs_1000_7.csv", header = F)
+
 
 set.seed(3733)
 cov_types <- c("exp", "gauss", "powexp", "matern3_2", "matern5_2")
 reg_forms <- c("~0", "~.", "~.^2")
 reg_names <- c("constant", "full_linear", "full_linear+int")
-num_smpls <- c(100, 200, 400, 800)
+doe_names <- c("srs", "lhs", "lhs-opt", "sobol")
+n_reps <- 25
+
+# Read pool of test data
+xx_tests  <- read.csv("../simulation/design/7params/lhs_1000_7.csv", header = F)
+y_tests <- evalOutputPiston(rescaleInputPiston(xx_pool))
+names(xx_tests) <- input_names
+
+reg_forms <- c("~0", "~.") 
+reg_names <- c("constant", "full_linear") 
+num_smpls <- c(16, 32) # 64, 128)
 doe_names <- c("srs", "lhs", "lhs-opt", "sobol")
 
+
 # Read the Data ---------
-q2_df <- data.frame(q2 = c(), 
-                    cov_type = c(),
-                    doe = c(),
-                    reg = c(),
-                    n = c())
+q2_piston <- data.frame(q2 = c(), 
+                        cov_type = c(),
+                        doe = c(),
+                        reg = c(),
+                        n = c())
+
+for (num_smpl in num_smpls)
+{
+    for (doe_name in doe_names)
+    {
+        # Loop over regression term
+        for (i in 1:length(reg_forms))
+        {
+            # Loop over covariance type
+            for (cov_type in cov_types)
+            {
+                # Loop over replications
+                for (j in 1:n_reps)
+                {
+                    # Read input
+                    train_filename <- paste(doe_name, "_", num_smpl, 
+                                            "_7_", j, ".csv", 
+                                           sep = "")
+                    train_fullname <- paste("../simulation/design/7params/", 
+                                           train_filename, 
+                                           sep = "")
+                    xx_train <- read.csv(train_fullname, header = F)
+                    # Evaluate output with rescaled input
+                    y_train <- evalOutputPiston(rescaleInputPiston(xx_train))
+                    # Give names to the input dataset
+                    names(xx_train) <- input_names
+                    
+                    # Train the model
+                    m <- km(as.formula(reg_forms[i]),
+                            design = xx_train, response = log(y_train), 
+                            covtype = cov_type,
+                            control = list(pop.size = 100, trace = T))
+                    
+                    # Make prediction using metamodel
+                    y_preds <- predict(m, newdata=xx_tests, type="UK")$mean
+                    
+                    # Compute Predictivity Coefficient
+                    q2_piston <- rbind(q2_piston,
+                                        data.frame(q2 = evalErmse(y_tests, exp(y_preds)), 
+                                                   cov_type = cov_type,
+                                                   doe = doe_name,
+                                                   reg = reg_names[i],
+                                                   n = num_smpl)) 
+                    
+                }
+            }
+        }
+        
+    }
+}
+
+p <- ggplot(data = subset(q2_piston, n == 32))
+p <- p + geom_point(mapping = aes(x=cov_type, y = q2)) + facet_grid(reg ~ doe)
+
+p <- p + geom_jitter(mapping = aes(x=cov_type, y = q2), width=0.10) + facet_grid(reg ~ doe)
+p
+
+reg <- "constant"
+doe <- "srs"
+plot(ecdf(rnorm(10)))
+plot(ecdf(subset(q2_piston, cov_type == "gauss" & reg == "constant" & doe == "srs")$q2), do.p=F)
+lines(ecdf(subset(q2_piston, cov_type == "gauss" & reg == "constant" & doe == "lhs")$q2), col = "red", do.p=F)
+lines(ecdf(subset(q2_piston, cov_type == "gauss" & reg == "constant" & doe == "lhs-opt")$q2), col = "red", do.p=F)
+lines(ecdf(subset(q2_piston, cov_type == "gauss" & reg == "constant" & doe == "sobol")$q2), col = "green", do.p=F)
+lines(ecdf(subset(q2_piston, cov_type == "powexp" & reg == "constant" & doe == "srs")$q2), do.p=F)
+lines(ecdf(subset(q2_piston, cov_type == "powexp" & reg == "constant" & doe == "lhs")$q2), col = "red", do.p=F)
+lines(ecdf(subset(q2_piston, cov_type == "powexp" & reg == "constant" & doe == "sobol")$q2), col = "green", do.p=F)
+lines(ecdf(subset(q2_piston, cov_type == "matern5_2" & reg == "constant" & doe == "srs")$q2), do.p=F)
+lines(ecdf(subset(q2_piston, cov_type == "matern5_2" & reg == "constant" & doe == "lhs")$q2), col = "red", do.p=F)
+lines(ecdf(subset(q2_piston, cov_type == "matern5_2" & reg == "constant" & doe == "sobol")$q2), col = "green", do.p=F)
+
+
+
+subset(q2_piston, cov_type == "gauss" & reg == "constant" & doe == "lhs")
+
+lines(ecdf(subset(q2_piston, cov_type == "exp" & reg == reg)$q2), col = col2rgb("#7fc97f"), do.p=F)
+lines(ecdf(subset(q2_piston, cov_type == "powexp" & reg == reg)$q2), col = "red", do.p=F)
+lines(ecdf(subset(q2_piston, cov_type == "matern3_2" & reg == reg)$q2), col = "green", do.p=F)
+lines(ecdf(subset(q2_piston, cov_type == "matern5_2" & reg == reg)$q2), col = "violet", do.p=F)
+
+plot(ecdf(subset(q2_piston, cov_type == "gauss" & reg == reg)$q2), do.p=F)
+
 for (doe_name in doe_names)
 {
     # Read input and a pool test data
