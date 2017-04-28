44397fc3d63c41e4b8e60b5b99e2e977395b38a3
Damar Wicaksono <damar.wicaksono@psi.ch>
2017-04-28 11:02:24 +0200

    Loop over PCs, exp.conf, and reps for Sandia Pr.
    - Innermost loop is the three principal component metamodels
    - Going out is the 5 experimental conditions
    - Afterward is the loop over the number of replications

--- analysis/sandia_thermal.R
+++ analysis/sandia_thermal.R
@@ -15,158 +15,136 @@ for (i in 1:2) input_names <- c(input_names, paste("x", i, sep = ""))
 
 xx_tests <- read.csv("../simulation/design/2params/lhs_5000_2.csv", header = F)
 names(xx_tests) <- input_names
-#y_tests <- evalOutputFranke(xx_tests)
 
-xx_train <- read.csv("../simulation/design/2params/srs_8_2_1.csv", header = F)
-names(xx_train) <- input_names
+
 
 # Training --------------
 t_s <- seq(0, 1000.0, by = 100)
 x0  <- 0.0
+T0 <- 25 # 
 q <- c(1000, 2000, 3000, 3500)
 L   <- c(1.27, 1.90, 2.54) * 1e-2
 
 yy_train <- evalOutputSandiaThermal(rescaleInputSandiaThermal(xx_train), 
                                     t_s, x0, 25, q[1], L[1])
 plot(t_s, yy_train[1,], ylim = c(25, 1200), type = "l")
-for (i in 2:16) lines(t_s, yy_train[i,])
+for (i in 2:8) lines(t_s, yy_train[i,])
 
 yy_tests <- evalOutputSandiaThermal(rescaleInputSandiaThermal(xx_tests), 
                                     t_s, x0, 25, q[1], L[1])
 
-# Carry out dimension reduction -----------------------------------------------
+cov_type <- "gauss"
+reg_form <- "~0"
 
-# SVD of temperature
-yy_scaled <- sweep(yy_train, 2, colMeans(yy_train)) / sd(yy_train)
-yy_svd <- svd(yy_scaled, nv = ncol(yy_scaled))
-(yy_svd$d / sqrt(15))^2 / sum((yy_svd$d / sqrt(15))^2)
-D <- diag(yy_svd$d)
-pc_scores <- yy_svd$u %*% D
 
-# Project test data using training principal component
-yy_tests_scaled <- sweep(yy_tests, 2, colMeans(yy_train)) / sd(yy_train)
-pcs_tests <- yy_tests_scaled %*% yy_svd$v
+# Experimental Configuration
+exp_confs <- list() 
+exp_confs[[1]] <- c(q[1], L[1]) 
+exp_confs[[2]] <- c(q[1], L[3])
+exp_confs[[3]] <- c(q[3], L[1])
+exp_confs[[4]] <- c(q[2], L[3])
+exp_confs[[5]] <- c(q[4], L[2])
 
-km_lists <- list()
-pc_scores_preds <- matrix(0, nrow = dim(xx_tests)[1], ncol = 3)
-# Training Kriging metamodel
-for (i in 1:3) 
+rec_rmse <- c()
+doe_name <- "srs"
+num_smpl <- 8
+num_reps <- 25
+# Loop over replications -----
+for (num_rep in 1:num_reps) 
 {
-    km_lists[[i]] <- km(as.formula("~0"),
-                        design = xx_train, response = pc_scores[,i], 
-                        covtype = "gauss",
-                        control = list(pop.size = 100, trace = F),
-                        nugget = 1e-8 * var(transf(y_train)))
-    pc_scores_preds[,i] <- predict(km_lists[[i]], newdata=xx_tests, type="UK")$mean
+    # Read input
+    train_filename <- paste(doe_name, "_", num_smpl, 
+                            "_2_", num_rep, ".csv", 
+                            sep = "")
+    train_fullname <- paste("../simulation/design/2params/", 
+                            train_filename, 
+                            sep = "")
+    xx_train <- read.csv(train_fullname, header = F)
+    # Give names to the input dataset
+    names(xx_train) <- input_names
+    # Evaluate output with rescaled input
+    y_train <- evalOutputFranke(xx_train)
+
+    rec_error <- c()
+    # Loop Over Configuration -----
+    for (exp_conf in exp_confs)
+    {
+        # Compute the raw test and training output
+        yy_tests <- evalOutputSandiaThermal(rescaleInputSandiaThermal(xx_tests), 
+                                            t_s, x0, T0, exp_conf[1], exp_conf[2])
+        
+        yy_train <- evalOutputSandiaThermal(rescaleInputSandiaThermal(xx_train), 
+                                            t_s, x0, T0, exp_conf[1], exp_conf[2])
+        
+        # Carry out SVD for Principal Component Analysis
+        # Centered and scaled the raw output (standardized)
+        yy_train_std <- scale(yy_train[,2:11], scale = T)
+        yy_tests_centered <- sweep(yy_tests[,2:11], MARGIN = 2, 
+                                   attr(yy_train_std, "scaled:center"), 
+                                   `-`)
+        yy_tests_std <- sweep(yy_tests_centered,
+                              MARGIN = 2,
+                              attr(yy_train_std, "scaled:scale"),
+                              `/`)# Remove the first column
+        # Carry Out SVD
+        yy_tests_svd <- svd(yy_tests_std)
+        yy_train_svd <- svd(yy_train_std)
+        # Construct Singular Values Matrix
+        D <- diag(yy_train_svd$d)
+        # Compute the Principal Component Scores
+        pcs_train <- yy_train_svd$u %*% D
+        # Project test data using the PC from training
+        pcs_tests <- yy_tests_std %*% yy_train_svd$v
+        
+        km_list <- list()
+        pcs_preds <- matrix(0, nrow = dim(xx_tests)[1], ncol = 3)
+        # Train the Kriging metamodel (Retain 3 based on a preli. analysis)
+        # Loop Over Principal Component metamodel -----
+        for (i in 1:3) 
+        {
+            # Train the metamodel
+            km_list[[i]] <- km(as.formula(reg_form),
+                               design = xx_train, response = pcs_train[,i], 
+                               covtype = cov_type,
+                               control = list(pop.size = 100, trace = F),
+                               nugget = 1e-8 * var(pcs_train[,i]))
+            # Compute the prediction on test data
+            pcs_preds[,i] <- predict(km_list[[i]], 
+                                     newdata=xx_tests, 
+                                     type="UK")$mean
+        }
+        
+        # Reconstruct time-dependent temperature
+        dev_matrix <- sweep(pcs_preds[,1:3] %*% t(yy_train_svd$v[,1:3]),
+                            MARGIN = 2,
+                            attr(yy_train_std, "scaled:scale"),
+                            `*`)
+        ave_matrix <- matrix(replicate(dim(xx_tests)[1], 
+                                       attr(yy_train_std, "scaled:center")),
+                             ncol = dim(yy_tests_std)[2], byrow = T)
+        rec_matrix <- ave_matrix + dev_matrix
+        
+        # Compute the reconstruction error
+        rec_error <- c(rec_error, yy_tests[,2:11] - rec_matrix)
+    }
+    rec_rmse <- c(rec_rmse, sqrt(mean(rec_error^2)))
 }
 
-# Compute Reconstruction Error both from PC and Kriging -----
-hot_matrix <- pc_scores_preds[,1:2] %*% t(yy_svd$v[,1:2]) * sd(yy_train)
-mean_vector <- apply(yy_train, 2, mean)
-rec_matrix <- matrix(replicate(dim(xx_tests)[1], mean_vector), ncol=11, byrow = T) + hot_matrix
-plot(t_s, yy_tests[1,])
-points(t_s, rec_matrix[1,], pch = 6)
-
-sqrt(mean(apply((yy_tests - rec_matrix)^2, 1, sum))) # Mean of Frobenius Norm
-
-
-evalRMSE(y_test = pcs_tests[,3], pc_scores_preds)
+# Exploratory Plot ------------------------------------------------------------
+plot(pcs_tests[,1], pcs_preds[,1])
 abline(0,1)
-plot(m)
-matrix(c(pcs_tests[1:10,1], pc_scores_preds[1:10]), nrow=10)
-yy_pca <- prcomp(yy)
-yy_svd$v
-yy_pca$sdev^2/sum(yy_pca$sdev^2)
-yy_svd$d^2 / sum(yy_svd$d^2)
+sqrt(mean(rec_error^2))
+# Compute the Reconstruction Error
+evalRMSE(y_test = pcs_tests[,1], pcs_preds[,1])
+evalRMSE(y_test = pcs_tests[,2], pcs_preds[,2])
+evalRMSE(y_test = pcs_tests[,3], pcs_preds[,3])
 
-(yy_svd$d / sqrt(31))^2 / sum((yy_svd$d / sqrt(31))^2)
+evalQ2(y_tests, y_preds)
 
-yy_svd$d[1]^2 / 31
-pc_scores[,1] * yy_svd$v[,1]
+plot(t_s, yy_tests[3,])
+lines(t_s[2:11], rec_matrix[3,])
+rec_matrix[1,]
+yy_tests[,2:11] - rec_matrix
+#(yy_train_svd$d / sqrt(7))^2 / sum((yy_train_svd$d / sqrt(7))^2)
 
-matrix(apply(yy, 2, mean), ncol=11) + matrix(pc_scores[,1], nrow=32) %*% matrix(yy_svd$v[,1], ncol = 11)
-mean_vector <- apply(yy, 2, mean)
-devi_vector <- (matrix(pc_scores[,1], nrow=32) %*% matrix(yy_svd$v[,1], ncol = 11) + 
-    matrix(pc_scores[,2], nrow=32) %*% matrix(yy_svd$v[,2], ncol = 11) + 
-    matrix(pc_scores[,3], nrow=32) %*% matrix(yy_svd$v[,3], ncol = 11)) * sd(yy)
-rec_matrix <- matrix(replicate(32, mean_vector), ncol=11,byrow = T) + devi_vector
-
-mean(sqrt(apply((yy - rec_matrix)^2,1, mean)))
-
-plot(t_s, yy[31,]) #, type = "l", ylim = c(-0.5, 0.5))
-points(t_s, rec_matrix[31,], pch=6)
-for (i in 2:32) lines(t_s, yy[i,])
-for (i in 2:32) lines(t_s, rec_matrix[i,],col="red")
-
-matrix(yy_svd$v[,1], nrow = 11)
-
-mean((yy[,] - apply(yy, 2, mean) - pc_scores[,1] * yy_svd$v[,1] * sd(yy))^2)
-(yy[1,] - apply(yy, 2, mean))
-sqrt(mean(apply(sweep(yy, 2, apply(yy, 2, mean))^2, 1, mean)))
-mean(sqrt(apply(sweep(yy, 2, apply(yy, 2, mean))^2, 1, mean)))
-
-plot(t_s, apply(yy, 2, mean) + pc_scores[1,1] * yy_svd$v[,1] * sd(yy)
-     + pc_scores[1,2] * yy_svd$v[,1] * sd(yy)
-     + pc_scores[1,3] * yy_svd$v[,1] * sd(yy))
-lines(t_s, yy[1,])
-
-plot(t_s, yy_scaled[1,], type = "l", ylim = c(-0.5, 0.5))
-for (i in 2:32) lines(t_s, yy_scaled[i,])
-
-yy_svd$d^2 / 31) / sum(yy_svd$d^2 / 31))[1:3])
-yy_svd <- svd(yy_scaled, nv = ncol(yy_scaled))
-sd2 <- (yy_svd$d / sqrt(31))^2 / sum(sd2)
-sd2
-pc_scores[,1]
-yy_scaled %*% yy_pca$rotation
-
-hist(pc_scores[,3])
-?pca
-?svd
-yy_centered
-yy[32,] -  apply(yy, 2, mean)
-yy_centered[32,]
-length(apply(yy, 2, mean))
-
-
-plot(t_s, yy[1,])
-for (i in 2:100) lines(t_s, yy[i,])
-series_sum <- 0
-for (j in 1:6) {
-    series_sum <- series_sum + 1/j**2 * 
-        exp(-1 * j**2 * pi**2 * xx[,1] / xx[,2] * t_s[1] / L[1]**2) * 
-        cos(j * pi * x0 / L[1])
-}
-series_sum <- series_sum * 2 / pi**2
-series_sum
-y<- replicate(100, 25) + q[1] * L[1] / xx[,1] * (xx[,1] / xx[,2] * t_s[1] / L[1]**2 + 1.0/3.0 - x0 / L[1] + 
-         0.5 * (x0/L[1])**2 - series_sum)
-
-y
-(xx[,1] / xx[,2] * t_s[1] / L[1]**2 + 1.0 / 3.0 - 0.5 * series_sum)
-
-
-exp(-1 * j**2 * pi**2 * xx[,1] / xx[,2] * t_s[1] / L[1]**2)
-
-
-series_sum <- 0
-series_sum <- series_sum + 1/1**2 * exp(-1 * 1**2 * pi**2 * xx[,1] / xx[,2] * t_s[10] / L[1]**2) * 
-    cos(1 * pi * x0 / L[1])
-series_sum <- series_sum * 2 / pi**2
-series_sum
-
-
--1 * 1**2 * pi**2 * xx[,1] / xx[,2] * t_s[10] / L[1]**2
-q[1] * L[1] / xx[,1] * (xx[,1] / xx[,2] * t_s[1] / L[1]**2 + 1.0/3.0 - x0 / L[1] + 0.5 * (x0/L[1])**2 - series_sum)
-xx
-q[1] * L[1] / xx[,1]
-
-xx[,1] / xx[,2] * t_s[1] / L[1]**2
-
-series_sum
-q[1] * L[1] * (xx[,1]/xx[,2] * t_s[10] / L[1]**2 + 1.0/3.0 - x0/L[1] + 0.5 * (x0/L[1])**2 - series_sum)
-q[1] * L[1] / xx[,1] * (xx[,1] / xx[,2] * t_s[i] / L[1]**2 + 1.0/3.0 - x0 / L[1] + 
-                            0.5 * (x0/L[1])**2 - series_sum)
-series_sum
-xx[,1]/xx[,2] * t_s[10] / L[1]**2 + 1.0/3.0 - x0/L[1] + 0.5 * (x0/L[1])**2 - series_sum
 
